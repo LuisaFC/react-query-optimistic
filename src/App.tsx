d37412e7602ed06617dd3f6e6a6a1a8ae0addd72
LuisaFC <luisabsb@gmail.com>
2024-12-16 12:00:35 -0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "./app/contexts/ThemeContext";
 import { Header } from "./components/Header";
+import { UserForm } from "./components/UserForm";
 import { UsersList } from "./components/UsersList";
 
 export function App() {
@@ -8,7 +9,8 @@ export function App() {
       <div className="max-w-[500px] mx-auto mt-20">
         <Header />
 
-        <main className="mt-10">
+        <main className="mt-10 space-y-3">
+          <UserForm />
           <UsersList />
         </main>
       </div>
