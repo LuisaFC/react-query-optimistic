fc8ed0329ed206be06881506f016d79f1a38fab9
LuisaFC <luisabsb@gmail.com>
2024-12-17 15:51:21 -0300

    Optimistic Update for  toggle

--- src/app/hooks/useUpdateUser.ts
+++ src/app/hooks/useUpdateUser.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateUser } from "../services/updateUser";
 import { USERS_QUERY_KEY } from "./useUsers";
+import { IUser } from "../types/IUser";
 
 
 export function useUpdateUser() {
@@ -8,10 +9,25 @@ export function useUpdateUser() {
 
   const {mutateAsync, isPending} = useMutation({
     mutationFn: updateUser,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: USERS_QUERY_KEY
-      })
+    onMutate: (variables) => {
+      const previousUsers = queryClient.getQueryData<IUser[]>(USERS_QUERY_KEY)
+
+      queryClient.setQueryData<IUser[]>(
+        USERS_QUERY_KEY,
+        (old) => old?.map(user => (
+          user.id === variables.id? {...user, ...variables} : user
+        ))
+      );
+      return { previousUsers }
+    },
+
+    onError: async(_error, _variables, context) => {
+      await queryClient.cancelQueries({queryKey: USERS_QUERY_KEY})
+
+      queryClient.setQueryData<IUser[]>(
+        USERS_QUERY_KEY,
+        context?.previousUsers
+      );
     }
   })
 
