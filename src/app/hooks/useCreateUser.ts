5947dca9e694881d7d7cd63fb3738317c9021efa
LuisaFC <luisabsb@gmail.com>
2024-12-17 14:55:10 -0300

    Manipulating directly in the cache

--- src/app/hooks/useCreateUser.ts
+++ src/app/hooks/useCreateUser.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createUser } from "../services/createUser";
 import { USERS_QUERY_KEY } from "./useUsers";
+import { IUser } from "../types/IUser";
 
 export const CREATE_USER_MUTATION_KEY = ['createUser']
 
@@ -8,12 +9,36 @@ export function useCreateUser() {
   const queryClient = useQueryClient()
 
   const {mutateAsync, isPending} = useMutation({
-    mutationKey: CREATE_USER_MUTATION_KEY,
     mutationFn: createUser,
-    onSuccess: () => {
-      return queryClient.invalidateQueries({
-        queryKey: USERS_QUERY_KEY
-      })
+    onMutate: (variables) => {
+      const tmpUserId = String(Math.random())
+
+      queryClient.setQueryData<IUser[]>(
+        USERS_QUERY_KEY,
+        (old) => old?.concat({
+          ...variables,
+          id: tmpUserId
+        })
+      );
+
+      return {tmpUserId}
+    },
+    onSuccess: (data, _variables, context) => {
+      queryClient.setQueryData<IUser[]>(
+        USERS_QUERY_KEY,
+        (old) => old?.map(user => (
+          user.id === context.tmpUserId? data : user
+        ))
+      );
+    },
+
+    onError: (_error, _variables, context) => {
+      queryClient.setQueryData<IUser[]>(
+        USERS_QUERY_KEY,
+        (old) => old?.filter(user => (
+          user.id !== context?.tmpUserId
+        ))
+      )
     }
   })
 
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -8,18 +8,16 @@ export function UserForm() {
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
 
-  const {createUser, isLoading} = useCreateUser();
+  const {createUser} = useCreateUser();
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault();
 
     try{
-      await createUser({name, username, blocked: false})
-
       setName('')
       setUsername('')
 
-      toast.success('Usuário cadastrado com sucesso')
+      await createUser({name, username, blocked: false})
     }
     catch{
       toast.error('Erro ao cadastrar usuário')
@@ -33,15 +31,13 @@ export function UserForm() {
         <Input placeholder="Nome do usuário"
           value={name}
           onChange={event => setName(event.target.value)}
-          disabled={isLoading}
         />
         <Input placeholder="@ do github"
           value={username}
           onChange={event => setUsername(event.target.value)}
-          disabled={isLoading}
         />
       </div>
-      <Button className="mt-3 w-full" disabled={isLoading}>Cadastrar</Button>
+      <Button className="mt-3 w-full">Cadastrar</Button>
     </form>
   );
 }
